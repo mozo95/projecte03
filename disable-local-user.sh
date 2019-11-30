0825dafb6f80b95e7c111da317cd749bbd7e986c
mozo95 <57413382+mozo95@users.noreply.github.com>
2019-11-30 19:56:26 +0100

    Update disable-local-user.sh

--- disable-local-user.sh
+++ disable-local-user.sh
@@ -32,10 +32,10 @@ while getopts ":dra" opt; do
 done
 
 # Make sure the script is being executed with superuser privileges.
-if [[ "${UID}" -eq "1" ]]
+if [[ "$EUID" -ne "0" ]]
 then
-        echo 'You dont have privileges.'
-    exit 1
+	echo "You dont have privileges."
+       	exit 1;
 fi
 
 # If the user doesn't supply at least one argument, give them help.
@@ -45,56 +45,59 @@ then
 fi
 
 # Loop through all the usernames supplied as arguments.
-for arg in "${@:2}"
+for arg in "${@}"
 do
-	# Make sure the user exists
-	a=$(id -u "$arg" 2>/dev/null)
-	if [[ $? -eq "0" ]]
-	then
-		echo "id: $arg: no such user"
-		exit 1
-	fi
-	# Make sure the UID of the account is at least 1000.
-	if [[ "$a" -lt "1000" ]]
-	then
-		echo "Refusing to remove the $arg account with UID $a"
-		exit 1;
-	fi
-	# Create an archive if requested to do so.
-	if [[ $archive -eq "1" ]]
-	then
-		# Make sure the ARCHIVE_DIR directory exists.
-		mkdir -p /home/ARCHIVE_DIR
-		# Archive the user's home directory and move it into the ARCHIVE_DIR
-		tar -zcvf "/home/${arg}.tar.gz" "/home/ARCHIVE_DIR/$arg"
-	fi
-	if [[ $delete -eq "1" ]]
-	then
-		# Delete the user.
-		userdel $arg
-		# Check to see if the userdel command succeeded.
-		if [[ $? -eq "0" ]]
-		then
-			# We don't want to tell the user that an account was deleted when it hasn't been.
-			echo "User: $arg deleted successfully."
-		fi
-	else
-		chage -E0 $arg
-		if [[ $? -eq "0" ]]
-                then
-                        # We don't want to tell the user that an account was disabled when it hasn't been.
-			echo "User: $arg disabled successfully."
-		fi
-	fi
-	if [[ $remove -eq "1" ]]
-	then
-		rm -r "/home/${arg}"
-		if [[ $? -eq "0" ]]
-                then
-                        # We don't want to tell the user that an account's directory was deleted when it hasn't been.
-                        echo "User: $arg directory deleted successfully."
-		fi
-	fi
-
-	echo "$arg"
+    if [[ $arg =~ [-][dra] ]]
+    then
+        echo "Flags used: $arg"
+    else 
+	    # Make sure the user exists
+	    a=$(id -u "$arg" 2>/dev/null)
+	    if [[ $? -eq "1" ]]
+	    then
+		    echo "id: $arg: no such user"
+		    exit 1
+	    fi
+	    # Make sure the UID of the account is at least 1000.
+	    if [[ "$a" -lt "1000" ]]
+	    then
+	    	echo "Refusing to remove $arg account with UID $a"
+	    	exit 1;
+	    fi
+	    # Create an archive if requested to do so.
+	    if [[ $archive -eq "1" ]]
+	    then
+	    	# Make sure the ARCHIVE_DIR directory exists.
+	       	mkdir -p "/home/ARCHIVE_DIR"
+	    	# Archive the user's home directory and move it into the ARCHIVE_DIR
+	    	tar -zcvf "/home/ARCHIVE_DIR/${arg}.tar.gz" "/home/$arg" &> /dev/null
+	    fi
+	    if [[ $delete -eq "1" ]]
+	    then
+	    	# Delete the user.
+	    	userdel $arg
+	    	# Check to see if the userdel command succeeded.
+	    	if [[ $? -eq "0" ]]
+	    	then
+	    		# We don't want to tell the user that an account was deleted when it hasn't been.
+	    		echo "User: $arg deleted successfully."
+	    	fi
+	    else
+	    	chage -E0 $arg
+	    	if [[ $? -eq "0" ]]
+            then
+                # We don't want to tell the user that an account was disabled when it hasn't been.
+		    	echo "User: $arg disabled successfully."
+		    fi
+	    fi
+	    if [[ $remove -eq "1" ]]
+	    then
+	    	rm -r "/home/${arg}" &> /dev/null
+	    	if [[ $? -eq "0" ]]
+            then
+                # We don't want to tell the user that an account's directory was deleted when it hasn't been.
+                echo "User: $arg directory deleted successfully."
+	    	fi
+	    fi
+    fi
 done
